093c602107a689ed5ce0a3330604ab54c83c490a
plcnk <romain.pluciennik@gmail.com>
2023-08-09 17:47:59 +0200

    Sweet, sweet colors

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -14,11 +14,11 @@ static const char *fonts[]          = { "Source Code Pro:Medium:pixelsize=13:ant
 static const char dmenufont[]       = "Source Code Pro:Regular:pixelsize=13:antialias=true";
 static const unsigned int baralpha    = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#1f1d45";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#f8f8f2";
+static const char col_gray4[]       = "#2f2942";
+static const char col_cyan[]        = "#ae81ff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
